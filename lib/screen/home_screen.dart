951a7e4671454737bdbc1deb029b289cf37a6eb8
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-07 12:53:15 -0300

    feat: adicionando input para entrada de texto

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  // Controlador para o campo de texto
+  final TextEditingController _numeroController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Leitor de números repetidos',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: const Text(
+            'Leitor de números repetidos',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          "Tela home",
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _numeroController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: 'Digite os números da nota'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                  onPressed: () {
+                    print("numero digitado ${_numeroController.text}");
+                  },
+                  child: const Text('Processar número'))
+            ],
+          ),
+        ));
   }
 }
